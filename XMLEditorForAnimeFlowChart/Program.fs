eff4223258c8b0f1b476b4c5f49291b38af8eba6
ChristophGra <christoph.grarock@gmail.com>
2021-03-11 14:48:02 +0100

    Complete reset

--- XMLEditorForAnimeFlowChart/Program.fs
+++ XMLEditorForAnimeFlowChart/Program.fs
@@ -1,36 +1,8 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
-open BasicData
-open PrintData
-open BasicData
-
-
-
-let modifyDecision state =
-  printDecisionsTree 0 (state.treebase::[])
-  state
-
-
-
-let updateState (state:state) input = 
-  match input with 
-  | "modifydecision" -> 
-    (true, modifyDecision state)
-  | "end" -> 
-    (false,state)
-  | _ -> 
-    (true,state)
+﻿open System
 
 
 [<EntryPoint>]
 let main argv =
-  let beginningQuestion:decisionText.T = decisionText.create "Do you want to watch anime"  
-  let mutable cont = true
-  let baseNode = decisionTree.createDecisionFromEmpty beginningQuestion
-  let mutable state = {treebase = baseNode; animeHashList = []}
-  while cont do
-    (cont,state) <- updateState state (Console.ReadLine())
     
     
   0 // return an integer exit code
