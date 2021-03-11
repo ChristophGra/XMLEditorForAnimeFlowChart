d8d0036462c4c2c6eee42360f6a2393a08c2e649
ChristophGra <christoph.grarock@gmail.com>
2021-03-11 17:31:21 +0100

    rebuilding from scratch

---
+++ XMLEditorForAnimeFlowChart/DataDefinition.fs
@@ -0,0 +1,11 @@
+﻿module DataDefinition
+
+type anime = {title:string; description:string; imagepath:string}
+type animeHash =
+  | Hash of string
+type animeWithHash = {animeHash:animeHash; anime:anime}
+type result = 
+  | Anime of animeHash
+  | Decision of decision
+and decision = {text:string; results: result list}
+type state = {root:decision; animeHashList: animeWithHash list}
---
+++ XMLEditorForAnimeFlowChart/DataOperations.fs
@@ -0,0 +1,69 @@
+﻿module DataOperations
+
+open Helpers
+open DataDefinition
+open System.Data.Common
+open Microsoft.VisualBasic.CompilerServices
+open System
+
+
+let chooseAnime result = 
+  match result with
+  | Anime a -> Some a
+  | Decision d -> None
+
+let chooseDecision result =
+  match result with 
+  | Anime a -> None
+  | Decision d -> Some d
+
+let filterList chooser results =
+  List.choose chooser results
+
+let filterAnime =
+  filterList chooseAnime
+
+let filterDecisions =
+  filterList chooseDecision
+
+
+
+let replaceAnimeHash oldAnime newAnime data=
+  if data = oldAnime.animeHash then
+    newAnime.animeHash 
+  else 
+    oldAnime.animeHash
+
+let replaceAnime oldAnime newAnime data: animeHash=
+  if data = oldAnime.animeHash then
+     newAnime.animeHash
+  else
+     data
+  
+let rec extractAllAnime decision =
+  match decision.results with
+  | [] -> []
+  | lst ->
+    let anime = filterAnime lst
+    let decisions = filterDecisions lst
+    List.map extractAllAnime decisions
+    |> List.fold (fun x y -> x @ y) anime 
+
+let doEitherAnimeOrDecisionAction animeAction decisionAction res =
+  match res with 
+  | Anime a -> 
+    result.Anime (animeAction a)
+  | Decision d -> 
+    result.Decision (decisionAction d)
+
+let rec UpdateAnimeInDecisions oldAnime newAnime decision =
+  let decisionAction = UpdateAnimeInDecisions oldAnime newAnime
+  let animeAction = replaceAnime oldAnime newAnime
+  let action = 
+    doEitherAnimeOrDecisionAction animeAction decisionAction
+
+  {decision with results = List.map action decision.results}
+
+let UpdateAnimeInHashList oldAnime newAnime hashList =
+  let action = replaceAnime oldAnime newAnime
+  List.map action hashList
